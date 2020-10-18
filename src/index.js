baa37bb2ea45331e939789c66e4cad915154d420
Artem_T <tuzovartem@gmail.com>
2020-10-18 14:43:59 +0400

    fear: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n<0){
+        n*=-1
+    }
+    result = result = n.toString().split('').reverse().map(elem => {
+		return +elem;
+	}).join('');
+
+
+    return result
 }
